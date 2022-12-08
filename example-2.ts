23aee101f99e8f2472c4a43c9d98044a2c4484ca
Alaa Bouayed <hello@alaabouayed.com>
2022-12-08 12:14:46 +0000

    Implement new feature

--- example-2.ts
+++ example-2.ts
@@ -1,5 +1,8 @@
-// TODO This can be make a lot more efficient
-function uglyCode(arg1: any, arg2: any): any {
+/**
+ * TODO: This should be refactored completely. It's causing a lot of
+ * development pain and slowing us down.
+ */
+function someCrazyLogic(arg1: any, arg2: any): any {
   if (typeof arg1 === "string") {
     if (typeof arg2 === "string") {
       // TODO We should fix this
